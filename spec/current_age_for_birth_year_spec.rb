5899e0ca30a71409f0c2759c350ca9ac9f50180c
Christine Adams <christine.adams21@gmail.com>
2020-03-21 00:00:44 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../current_age_for_birth_year.rb'
+require_relative '../how_old_are_you.rb'
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
